19642ec4b1755c236d1ceb8b79f2dd0d7c419aff
Andy Ding <andy.ding@facilityone.cn>
2014-10-18 16:41:17 +0800

    rm -all

--- App/Conf/routersAdmin.php
+++
@@ -1,6 +0,0 @@
-<?php
-return array(
-        'URL_ROUTE_RULES_ADMIN' => array( //定义路由规则
-            "ddaadmin"=>"admin/Index/"
-        )
-);
--- App/Modules/Home/Action/TaskAction.class.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * 任务
- * @author dinglin
- *
- */
-class TaskAction extends BaseAction{
-    
-}
